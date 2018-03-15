1faf9090351343be02faf4769874753f47391c0c
Bundit Nuntates <silkyland@gmail.com>
2018-03-15 12:00:59 +0700

    edit unitest

--- tests/SawasdeeTest.php
+++ tests/SawasdeeTest.php
@@ -1,16 +1,17 @@
 <?php
-use \Silkyland\Sawasdee\Sawasdee;
+use \Silkyland\Sawasdee;
+
 /**
  * Created by PhpStorm.
  * User: Bundit Nuntaes
- * Date: 8/14/2015
- * Time: 4:14 PM
+ * Date: 15/03/2018
+ * Time: 12:00 AM
  */
 class SawasdeeTest extends PHPUnit_Framework_TestCase
 {
     public function testSilkylandToThaiDateTime()
     {
         $sawasdee = new Sawasdee;
-        $this->assertEquals('หนึ่งร้อยยี่สิบสามบาทถ้วน',$sawasdee->readThaiCurrency('123'));
+        $this->assertEquals('หนึ่งร้อยยี่สิบสามบาทถ้วน', $sawasdee->readThaiCurrency('123'));
     }
 }
